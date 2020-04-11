03438f0402a1272d992ff81299a957f3efec83cb
sethu <sethupathy005@gmail.com>
2020-04-11 14:21:43 +0530

    Initial Setup

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+gulp.task('sass' , function(){
+	return gulp.src('app/scss/style.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.series(['sass'])); 
+  
+})
